7190dc1ddcecfc6098e1cff560e0240cfa62831b
Samuel Weckstrom <samuel.weckstrom@gmail.com>
2021-06-04 22:01:03 +0200

    feat: update project demo

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -5,11 +5,23 @@ import {
   useRecordWebcam,
   CAMERA_STATUS,
 } from 'react-record-webcam';
-import type { WebcamRenderProps, RecordWebcamHook } from 'react-record-webcam';
+import type {
+  WebcamRenderProps,
+  RecordWebcamOptions,
+  RecordWebcamHook,
+} from 'react-record-webcam';
 import './styles.css';
 
+const OPTIONS: RecordWebcamOptions = {
+  filename: 'test-filename',
+  fileType: 'mp4',
+  width: 1920,
+  height: 1080,
+  aspectRatio: 1.777777778,
+};
+
 const App = () => {
-  const recordWebcam: RecordWebcamHook = useRecordWebcam();
+  const recordWebcam: RecordWebcamHook = useRecordWebcam(OPTIONS);
 
   const getRecordingFileHooks = async () => {
     const blob = await recordWebcam.getRecording();
@@ -22,63 +34,6 @@ const App = () => {
 
   return (
     <div>
-      <div className="demo-section">
-        <h1>Component demo</h1>
-        <RecordWebcam />
-      </div>
-      <div className="demo-section">
-        <RecordWebcam
-          render={(props: WebcamRenderProps) => {
-            return (
-              <div>
-                <h1>Component render prop demo</h1>
-                <p>Camera status: {props.status}</p>
-                <div>
-                  <button
-                    disabled={props.isWebcamOn || props.isPreview}
-                    onClick={props.openCamera}
-                  >
-                    Open camera
-                  </button>
-                  <button
-                    disabled={!props.isWebcamOn || props.isPreview}
-                    onClick={props.closeCamera}
-                  >
-                    Close camera
-                  </button>
-
-                  <button
-                    disabled={
-                      !props.isWebcamOn || props.isRecording || props.isPreview
-                    }
-                    onClick={props.start}
-                  >
-                    Start recording
-                  </button>
-                  <button disabled={!props.isRecording} onClick={props.stop}>
-                    Stop recording
-                  </button>
-                  <button disabled={!props.isPreview} onClick={props.retake}>
-                    Retake
-                  </button>
-                  <button disabled={!props.isPreview} onClick={props.download}>
-                    Download
-                  </button>
-                  <button
-                    disabled={!props.isPreview}
-                    onClick={async () => {
-                      const blob = await props.getRecording();
-                      getRecordingFileRenderProp(blob);
-                    }}
-                  >
-                    Get recording blob
-                  </button>
-                </div>
-              </div>
-            );
-          }}
-        />
-      </div>
       <div className="demo-section">
         <h1>Hooks demo</h1>
         <p>Camera status: {recordWebcam.status}</p>
@@ -161,6 +116,67 @@ const App = () => {
           autoPlay
           muted
           loop
+        />
+      </div>
+      <div className="demo-section">
+        <h1>Component demo</h1>
+        <RecordWebcam options={OPTIONS} />
+      </div>
+      <div className="demo-section">
+        <RecordWebcam
+          options={OPTIONS}
+          render={(props: WebcamRenderProps) => {
+            const showOpenCamera =
+              props.status !== CAMERA_STATUS.OPEN &&
+              props.status !== CAMERA_STATUS.RECORDING &&
+              props.status !== CAMERA_STATUS.PREVIEW;
+            const showCloseCamera =
+              props.status === CAMERA_STATUS.OPEN || CAMERA_STATUS.RECORDING;
+            const showStart = props.status === CAMERA_STATUS.OPEN;
+            const showStop = props.status === CAMERA_STATUS.RECORDING;
+            const showRetake = props.status === CAMERA_STATUS.PREVIEW;
+            const showDownload = props.status === CAMERA_STATUS.PREVIEW;
+
+            return (
+              <div>
+                <h1>Component render prop demo</h1>
+                <p>Camera status: {props.status}</p>
+                <div>
+                  <button disabled={!showOpenCamera} onClick={props.openCamera}>
+                    Open camera
+                  </button>
+                  <button
+                    disabled={showOpenCamera || showRetake || !showCloseCamera}
+                    onClick={props.closeCamera}
+                  >
+                    Close camera
+                  </button>
+
+                  <button disabled={!showStart} onClick={props.start}>
+                    Start recording
+                  </button>
+                  <button disabled={!showStop} onClick={props.stop}>
+                    Stop recording
+                  </button>
+                  <button disabled={!showRetake} onClick={props.retake}>
+                    Retake
+                  </button>
+                  <button disabled={!showDownload} onClick={props.download}>
+                    Download
+                  </button>
+                  <button
+                    disabled={!showDownload}
+                    onClick={async () => {
+                      const blob = await props.getRecording();
+                      getRecordingFileRenderProp(blob);
+                    }}
+                  >
+                    Get recording blob
+                  </button>
+                </div>
+              </div>
+            );
+          }}
         />
       </div>
     </div>
