914e21201b3e113df3eefc2abe56f88b8285c056
Samuel Weckstrom <samuel.weckstrom@gmail.com>
2024-03-02 15:21:59 +0100

    chore(demo): update build script

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: true,
+  },
+  build: {
+    outDir: './dist',
   },
 });
